f5441dd46093186006f49b53d488e54511d40111
Ericcentrik <enaiman1@gmail.com>
2021-08-22 14:35:17 -0400

    fix typo

--- server.js
+++ server.js
@@ -91,6 +91,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
-app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
